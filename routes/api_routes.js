d19cb8480260c1982ad8f04cd06fa2ee4cbca14a
Kathleen Welch <ktywelch@gmail.com>
2021-03-12 14:57:46 -0800

    removed auth from getting saved because of performance issues

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -3,7 +3,7 @@ const auth = require("../middleware/auth");
 const { getBooks, getBooksTag, updateBook, delBook, createBook } = require("../controllers/ApiController");
 
 router.get('/books', auth, getBooks);
-router.get('/booksTag/:id', auth, getBooksTag);
+router.get('/booksTag/:id',  getBooksTag);
 router.put('/books/:id', auth, updateBook);
 router.delete('/books/:id',auth, delBook);
 router.post('/books', auth, createBook);
