c45006d3d06f6bd0a77e3305084c226b2990fc90
Kathleen Welch <ktywelch@gmail.com>
2021-03-12 15:26:39 -0800

    removed auth from getting saved because of performance issues

--- client/src/components/pages/Saved.js
+++ client/src/components/pages/Saved.js
@@ -11,9 +11,10 @@ const Saved = () => {
     const { userData, setUserData } = useContext(UserContext);
     const history = useHistory();
 
-    const [data, setData] = useState(null);
-    const [isError, setIsError] = useState(null);
-    const [isPending, setIsPending] = useState(true);
+    const {data, isError, isPending} = useAxios(url);
+    // const [data, setData] = useState(null);
+    // const [isError, setIsError] = useState(null);
+    // const [isPending, setIsPending] = useState(true);
 
     useEffect(() => {
         if (!userData.user) history.push("/login");
@@ -22,65 +23,65 @@ const Saved = () => {
       const url = '/api/booksTag/'  + userData.user.id;
       console.log(url);
 
-/// need to fix this not sure impace of using the x-auth-token everywhere tink it is fine ...
-    useEffect(() => {
-        // setting up to catch an abort in the query
-        const CancelToken = axios.CancelToken;
-        const source = CancelToken.source();
+// /// need to fix this not sure impace of using the x-auth-token everywhere tink it is fine ...
+//     useEffect(() => {
+//         // setting up to catch an abort in the query
+//         const CancelToken = axios.CancelToken;
+//         const source = CancelToken.source();
         
-        /*   try {
-            const { data } = await axios.post("/users/login", form);
+//         /*   try {
+//             const { data } = await axios.post("/users/login", form);
       
-            setUserData({
-              token: data.token,
-              user: data.user,
-            });
+//             setUserData({
+//               token: data.token,
+//               user: data.user,
+//             });
       
-            localStorage.setItem("auth-token", data.token);
-            history.push("/");
-          } catch (err) {
-              */
+//             localStorage.setItem("auth-token", data.token);
+//             history.push("/");
+//           } catch (err) {
+//               */
 
 
             
 
-            axios.get(url, {
-                cancelToken: source.token,
-                responseType: 'json',
-                headers: { "x-auth-token": localStorage.getItem("auth-token")}
-                })
-                .then(res => {
-                    if(res.status !== 200){
-                     throw Error("Did not get valid for that resource")
-                    }
-                return res.data;
-                })
-                .then ((data) => {
-                console.log(data);
-                setData(data);
-                setIsPending(false);
-                setIsError(null);
-                })
-                .catch((err) => {
-                    console.log(err.message);
-                    if (axios.isCancel.err) {
-                        console.log('Request canceled', err.message);
-                      }  else {    
-                    setIsError(err.message);
-                    setIsPending(false);
-                    }
-                })
-        // return () => CancelToken.cancel;
-    },[url]) 
+//     //         axios.get(url, {
+//     //             cancelToken: source.token,
+//     //             responseType: 'json',
+//     //             headers: { "x-auth-token": localStorage.getItem("auth-token")}
+//     //             })
+//     //             .then(res => {
+//     //                 if(res.status !== 200){
+//     //                  throw Error("Did not get valid for that resource")
+//     //                 }
+//     //             return res.data;
+//     //             })
+//     //             .then ((data) => {
+//     //             console.log(data);
+//     //             setData(data);
+//     //             setIsPending(false);
+//     //             setIsError(null);
+//     //             })
+//     //             .catch((err) => {
+//     //                 console.log(err.message);
+//     //                 if (axios.isCancel.err) {
+//     //                     console.log('Request canceled', err.message);
+//     //                   }  else {    
+//     //                 setIsError(err.message);
+//     //                 setIsPending(false);
+//     //                 }
+//     //             })
+//     //     // return () => CancelToken.cancel;
+//     // },[url]) 
 
   
-    console.log(data);
+//     console.log(data);
 
     return (
         <div>
             <span>List of books saved </span> 
-            {/* {url && isError && <div>No Data - {isError}</div> }
-            {isPending && <div>Data is Loading ...</div> } */}
+            {url && isError && <div>No Data - {isError}</div> }
+            {isPending && <div>Data is Loading ...</div> }
             {data &&  <SavedDisplay books={data} calledby={'saved'} />}
         </div>
     )    
